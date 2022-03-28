861bd4316d97432c959e69ec2958bbf67d236f74
Ionut Balica <balicaionut@gmail.com>
2022-03-28 16:46:34 +0300

    db created

--- database.py
+++ database.py
@@ -1,4 +1,8 @@
 from cgi import test
 import sqlite3
 
-test
+connection = sqlite3.connect("data.db")
+
+with connection:
+    connection.execute("CREATE TABLE beans (id INTEGER PRIMARY KEY, name TEXT, method TEXT, rating INTEGER);")
+    
